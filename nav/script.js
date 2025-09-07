c1b29faef232a0c526ea19a4819e93b26ab862d4
julieannedelosreyes18 <julieannedelosreyes18@gmail.com>
2025-09-07 18:30:48 +0800

    Update for key-kong repo

--- nav/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMobileMenu() {
-    const menu = document.getElementById('mobileMenu');
-    menu.classList.toggle('hidden');
-  }
-
-  function toggleSubMenu(id) {
-    const submenu = document.getElementById(id);
-    submenu.classList.toggle('hidden');
-  }
